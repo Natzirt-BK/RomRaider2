3ba9735e15fed6ce0fbcaf35887f6c3ed09ba05e
Natzirt-BK <bukenbergertristan@gmail.com>
2026-09-04 20:04:16 -0700

    Use a platform absolute path in logger profile storage test

--- src/test/java/com/romraider/logger/runtime/LoggerProfileStorageTest.java
+++ src/test/java/com/romraider/logger/runtime/LoggerProfileStorageTest.java
@@ -4,16 +4,18 @@ package com.romraider.logger.runtime;
 import static org.junit.Assert.assertEquals;
 
 import java.nio.file.Paths;
+import java.nio.file.Path;
 
 import org.junit.Test;
 
 public class LoggerProfileStorageTest {
     @Test
     public void backupProfileBelongsToPackagedSettingsDirectory() {
-        assertEquals(Paths.get("/opt/romraider2/config/user/profiles/"
-                        + "profile_backup.xml"),
-                LoggerProfileStorage.backupPath(
-                        Paths.get("/opt/romraider2/config/user")));
+        Path settingsDirectory = Paths.get("romraider2", "config", "user")
+                .toAbsolutePath();
+        assertEquals(settingsDirectory.resolve("profiles")
+                        .resolve("profile_backup.xml"),
+                LoggerProfileStorage.backupPath(settingsDirectory));
     }
 
     @Test(expected = IllegalArgumentException.class)
